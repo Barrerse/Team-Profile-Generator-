83a76a453b98eb30fe922c2a05ac06ac374e646a
Barrerse <sebastianbarrera@live.com>
2022-12-07 22:50:58 -0500

    Successfully tested

--- index.js
+++ index.js
@@ -1,12 +1,13 @@
 const Manager = require("./lib/Manager.js")
 const Engineer = require("./lib/Engineer.js") 
 const Intern = require("./lib/Intern.js")
-const inquirer = require("inquirer");
+const inquirer = require('inquirer');
 const path = require ("path");
 const fs = require("fs");
 const OUTPUT_DIR = path.resolve(_dirname, "output");
 const outputPath = path.join(OUTPUT_DIR, "team.html");
 const generateTeam = require("./src/main.js")
+
 
 teamArray = [];
 
