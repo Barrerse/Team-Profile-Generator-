a5c32db9144e5e96461519b26b44182a8d6db7ef
Barrerse <sebastianbarrera@live.com>
2022-12-07 20:25:50 -0500

    Added the main constructor function

--- index.js
+++ index.js
@@ -7,3 +7,19 @@ const fs = require("fs");
 const OUTPUT_DIR = path.resolve(_dirname, "output");
 const outputPath = path.join(OUTPUT_DIR, "team.html");
 const generateTeam = require("./src/template.js")
+
+teamArray = [];
+
+// Main Function 
+function runApp () {
+    
+    //Create Team constructor 
+    function createTeam () {
+        inquirer.prompt([{
+            type: "list",
+            message: "Enter the type of employee you want to add:"
+            name: "addEmployeePrompt"
+            choices: ["Intern","Engineer","Manager","Back"]
+        }])
+    }
+}
